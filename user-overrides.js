bf929e2554b6634f87c80071c3426e9b6997c25b
Martin <martin@example.com>
2023-09-30 00:51:25 +0200

    arkenfox

---
+++ user-overrides.js
@@ -0,0 +1,11 @@
+user_pref("signon.rememberSignons", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.screenshots.disabled", true);
+user_pref("devtools.accessibility.enabled", false);
+user_pref("browser.translations.enable", false);
+user_pref("identity.fxaccounts.enabled", false);
